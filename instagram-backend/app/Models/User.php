2b6777158d7d467ab8d1e0ca2eff9a53cc32462f
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-11 16:47:57 +0300

    User

--- instagram-backend/app/Models/User.php
+++ instagram-backend/app/Models/User.php
@@ -63,4 +63,8 @@ class User extends Authenticatable implements JWTSubject
     {
         return []; 
     }
+
+    public function posts(){
+        return $this->hasMany(InstaPost::class);
+    }
 }
