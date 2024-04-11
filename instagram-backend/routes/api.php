1be6e026a81cea6b3f8b7958f40c49b10b4db9af
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-04-11 16:48:51 +0300

    add tables

--- instagram-backend/routes/api.php
+++ instagram-backend/routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\InstaPostController;
+use App\Http\Controllers\PostssController;
+use App\Http\Controllers\PostsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Models\InstaPost;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +19,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['middleware' => 'jwt.auth'], function () {
+
 });
+
+Route::post('/create', [InstaPostController::class, 'createPosts']);
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+
+    Route::get('/get_users', [UserController::class, 'getUsers']);
+    Route::get('/get_posts',[InstaPostController::class,'getposts']);
+    
+   
